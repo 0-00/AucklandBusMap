2d85db9a0d858c062a498ea3781b10867234fba1
0-00 <scottcaldwell1397@gmail.com>
2022-06-18 17:14:59 +1200

    feat: store anchor points in quadtree

--- src/point_interface.js
+++ src/point_interface.js
@@ -1,18 +1,34 @@
 const csv = require('csv-parser');
 const fs = require('fs');
-
-let anchor_points = []
+const {QuadTree, Box, Point, Circle} = require('js-quadtree');
 
 function load_anchor_points () {
-    fs.createReadStream('bus_map_dots.csv')
-        .pipe(csv())
-        .on('data', (row) => {
-            anchor_points.push(row);
-        })
-        .on('end', () => {
-            console.log('Anchor points loaded');
-            resolve(ret)
+    let anchor_points = []
+    max_x, max_y, min_x, min_y = -Infinity, -Infinity, Infinity, Infinity
+
+    return new Promise((resolve, reject) => {
+        fs.createReadStream('bus_map_dots.csv')
+            .pipe(csv())
+            .on('data', (row) => {
+                max_x = max(row.x, max_x);
+                max_y = max(row.y, max_y);
+                min_x = min(row.x, min_x);
+                min_y = min(row.y, min_y);
+
+                anchor_points.push(row);
+            })
+            .on('end', () => {
+                console.log('Anchor points loaded');
+                resolve(ret)
+            });
+    }).then(() => {
+        const quadtree = new QuadTree(new Box(min_x, min_y, max_x, max_y));
+        anchor_points.forEach((element) => {
+            quadtree.insert(element);
         });
+        console.log('Quadtree populated');
+    });
+    
 }
 
 module.exports = {

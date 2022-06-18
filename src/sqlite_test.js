258291b2b57df73ac7b9d9ffeae2d4981ba93ccb
0-00 <scottcaldwell1397@gmail.com>
2022-06-18 14:32:48 +1200

    chore: tidied sqlite_test

--- src/sqlite_test.js
+++ src/sqlite_test.js
@@ -1,17 +1,25 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database('../bus_speeds.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err && err.code == "SQLITE_CANTOPEN") {
+        console.log("Can't open database...")
+        return;
+    } else if (err) {
+        console.log("Getting error " + err);
+    }
+    console.log("Database connected.")
+});
 
 function create() {
-    db.run("CREATE TABLE speeds (node_id, time, speed, bus_route_id)", (err) => {
+    db.run("CREATE TABLE speeds (node_id, time, speed, bearing, bus_route_id)", (err) => {
         if (err) return console.log(err.message);
         console.log("table created")
     });
 }
 
-function add_entry (node_id, time, speed, bus_route_id) {
-    const sql = `INSERT INTO speeds(node_id, time, speed, bus_route_id) VALUES(?,?,?,?)`
+function add_entry (node_id, time, speed, bearing, bus_route_id) {
+    const sql = `INSERT INTO speeds(node_id, time, speed, bearing, bus_route_id) VALUES(?,?,?,?,?)`
     db.serialize(() => {
-        db.run(sql, [node_id, time, speed, bus_route_id], (err) => {
+        db.run(sql, [node_id, time, speed, bearing, bus_route_id], (err) => {
             if (err) return console.log(err.message);
             // console.log("entry added")
         })
@@ -31,6 +39,7 @@ function print_table() {
 
 function query_node_avg (node_id) {
     const sql = `SELECT speed FROM speeds WHERE node_id=?`
+    ret = 0;
     db.all(sql, [node_id], (err, rows) => {
         if (err) return console.log(err.message);
 
@@ -38,8 +47,9 @@ function query_node_avg (node_id) {
         rows.forEach((row) => {
             i += row["speed"]
         })
-        console.log(i / rows.length);
+        ret = i / rows.length
     })
+    return ret;
 }
 
 function end() {
@@ -49,13 +59,13 @@ function end() {
     });
 }
 
-create();
+// create();
 
 for (var i = 0; i <= 50; i++) {
-    add_entry(0, new Date().getTime(), i, 0);
+    add_entry(0, new Date().getTime(), i, 0, 0);
 }
 
 // print_table();
-query_node_avg(0);
+console.log("avg speed:", query_node_avg(0));
 
 end();
